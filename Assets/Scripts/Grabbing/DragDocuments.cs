264d96fe27da82686f92cab462b4cb6a8dc70921
EfeCekirge <efecekirge@gmail.com>
2023-04-04 15:01:16 -0400

    Drag Papers Around

---
+++ Assets/Scripts/Grabbing/DragDocuments.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDocuments : MonoBehaviour
+{
+    IGrabbable grabbedObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit raycastHit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out raycastHit, 10f))
+            {
+                if (raycastHit.transform != null)
+                {
+                    if (raycastHit.collider.gameObject.TryGetComponent(out IGrabbable grabbableObject))
+                    {
+                        Vector3 screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+ 
+                        Vector3 offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+                        grabbableObject.Grabbed(gameObject, offset);
+                        grabbedObject = grabbableObject;
+                    }
+                }
+            }
+
+        }
+
+        if (Input.GetMouseButtonUp(0) && grabbedObject != null)
+        {
+            grabbedObject.Dropped();
+            grabbedObject = null;
+        }
+    }
+}
