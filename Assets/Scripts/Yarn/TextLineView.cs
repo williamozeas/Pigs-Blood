63a5397b95c06956cecf9a33b543d00f3d82dcf2
Will Ozeas <willatme@me.com>
2023-04-26 11:44:55 -0400

    New Dialogue box UI

--- Assets/Scripts/Yarn/TextLineView.cs
+++ Assets/Scripts/Yarn/TextLineView.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,7 +13,11 @@ public class TextLineView : LineView
     private string currentLine;
     private int charIndex;
 
+    private string prevCharacterName = "????";
+
     [SerializeField] private Image ShepherdDialogueBox;
+    [SerializeField] private TextMeshProUGUI CharacterNameText;
+    [SerializeField] private TextMeshProUGUI tmp;
     private Coroutine FadeCR;
     public override void RunLine(LocalizedLine dialogueLine, Action onDialogueLineFinished)
     {
@@ -28,15 +33,17 @@ public class TextLineView : LineView
             {
                 StopCoroutine(FadeCR);
             }
-            FadeCR = StartCoroutine(FadeDialogueBox(1, fadeTime));
+            FadeCR = StartCoroutine(FadeDialogueBox(1, 0.45f, fadeTime));
+            CharacterNameText.text = prevCharacterName;
         }
         else
         {
+            prevCharacterName = dialogueLine.CharacterName;
             if (FadeCR != null)
             {
                 StopCoroutine(FadeCR);
             }
-            FadeCR = StartCoroutine(FadeDialogueBox(0, fadeTime));
+            FadeCR = StartCoroutine(FadeDialogueBox(0, 1f, fadeTime));
         }
     }
 
@@ -55,23 +62,26 @@ public class TextLineView : LineView
         AudioManager.Instance.ContinueTextSound();
     }
 
-    private IEnumerator FadeDialogueBox(float goalAlpha, float time)
+    private IEnumerator FadeDialogueBox(float goalAlphaBox, float goalAlphaName, float time)
     {
         float timeElapsed = 0;
-        float startAlpha = ShepherdDialogueBox.color.a;
-        float diff = goalAlpha - startAlpha;
-        if (Mathf.Abs(diff) < 0.1f)
+        float startAlphaBox = ShepherdDialogueBox.color.a;
+        float diffBox = goalAlphaBox - startAlphaBox;
+        float startAlphaName = CharacterNameText.color.a;
+        float diffName = goalAlphaName - startAlphaName;
+        if (Mathf.Abs(diffBox) < 0.1f)
         {
             yield break;
         }
         while (timeElapsed < time)
         {
             float percent = EasingFunction.EaseOutQuad(0, 1, timeElapsed / time);
-            ShepherdDialogueBox.color = new Color(1, 1, 1, startAlpha + percent * diff);
-            Debug.Log(startAlpha + percent * diff);
+            ShepherdDialogueBox.color = new Color(1, 1, 1, startAlphaBox + percent * diffBox);
+            tmp.color = new Color(1, 1, 1, startAlphaName + percent * diffName);
             timeElapsed += Time.deltaTime;
             yield return null;
         }
-        ShepherdDialogueBox.color = new Color(1, 1, 1, goalAlpha);
+        ShepherdDialogueBox.color = new Color(1, 1, 1, goalAlphaBox);
+        tmp.color = new Color(1, 1, 1, goalAlphaName);
     }
 }
