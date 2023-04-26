1f67c7745333a5f39c3404247044b49a6bf867d3
Will Ozeas <willatme@me.com>
2023-04-25 22:13:00 -0400

    Typewriter sound effects

--- Assets/Scripts/Yarn/TextLineView.cs
+++ Assets/Scripts/Yarn/TextLineView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,5 +6,27 @@ using Yarn.Unity;
 
 public class TextLineView : LineView
 {
-    
+    private string currentLine;
+    private int charIndex;
+    public override void RunLine(LocalizedLine dialogueLine, Action onDialogueLineFinished)
+    {
+        base.RunLine(dialogueLine, onDialogueLineFinished);
+        currentLine = dialogueLine.RawText;
+        charIndex = 0;
+    }
+
+    public void PlayTextSFX()
+    {
+        if (currentLine[charIndex] != ' ')
+        {
+            AudioManager.Instance.CharacterTypedSound();
+        }
+        charIndex++;
+    }
+
+    public override void UserRequestedViewAdvancement()
+    {
+        base.UserRequestedViewAdvancement();
+        AudioManager.Instance.ContinueTextSound();
+    }
 }
